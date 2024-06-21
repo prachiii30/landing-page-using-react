8c787ee937cce6e80c2958e42cea17713f355baa
Prachi Parihar <prachisingh30aabb@gmail.com>
2024-06-21 12:04:11 +0530

    Add Files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
-import Home from './components/Home';
+import "./App.css";
+import Home from "../Components/Home.js";
+import About from "../components/About.js";
+import Work from "../Components/Work.js";
+import Testimonial from "../components/Testimonial.js";
+import Contact from "../components/Contact.js";
+import Footer from "../components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Home />
+      <About />
+      <Work />
+      <Testimonial />
+      <Contact />
+      <Footer />
     </div>
   );
 }
